7f2289db3eb612664e5da3c0974a9843ace0ee9f
Paul Metcalfe <paul.metcalfe+defode@gmail.com>
2012-12-21 19:31:24 +0000

    Update defode/functions.py
    Add definition of __all__ and define pow.

--- defode/functions.py
+++ defode/functions.py
@@ -1,8 +1,16 @@
 from defode.variable import function_factory
 
+"""
+Reflect some common C functions back into defode.
+"""
+
+__all__ = ['sin', 'cos', 'exp', 'log',
+           'expm1', 'log1p', 'pow']
+
 sin = function_factory('sin')
 cos = function_factory('cos')
 exp = function_factory('exp')
 log = function_factory('log')
 expm1 = function_factory('expm1')
 log1p = function_factory('log1p')
+pow = function_factory('pow')
