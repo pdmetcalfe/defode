823c276acde293c469ac5a4a4a1570f414abaec7
Paul Metcalfe <paul.metcalfe+defode@gmail.com>
2013-04-27 14:38:32 +0100

    Define the numbers of arguments for the common functions

--- defode/functions.py
+++ defode/functions.py
@@ -7,10 +7,10 @@ Reflect some common C functions back into defode.
 __all__ = ['sin', 'cos', 'exp', 'log',
            'expm1', 'log1p', 'pow']
 
-sin = function_factory('sin')
-cos = function_factory('cos')
-exp = function_factory('exp')
-log = function_factory('log')
-expm1 = function_factory('expm1')
-log1p = function_factory('log1p')
-pow = function_factory('pow')
+sin = function_factory('sin', 1)
+cos = function_factory('cos', 1)
+exp = function_factory('exp', 1)
+log = function_factory('log', 1)
+expm1 = function_factory('expm1', 1)
+log1p = function_factory('log1p', 1)
+pow = function_factory('pow', 2)
